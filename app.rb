24c8237b8c2216f9b7959caa02f0496708d025f8
Strand—Personal <strand-home@dhcp120.sea01.bbgcorp>
2012-05-05 13:41:53 -0700

    Added basic routing functionality and a list of coffees.

--- app.rb
+++ app.rb
@@ -9,3 +9,24 @@ require 'sinatra/reloader' if development?
 get '/' do
   erb :index
 end
+
+get '/shops' do
+  "Hello World"
+end
+
+get '/shop/:id' do
+end
+
+get '/shop/:id/baristas' do
+end
+
+get '/shop/:id/barista/:name' do
+end
+
+get '/shop/:id/coffees' do
+  erb :coffees
+end
+
+get '/baristas' do
+  erb :baristas
+end
